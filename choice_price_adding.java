e65b99a407cdf8ff8eb91302a3bd39b574c7c7d6
Anuprita Rajput <74887568+Anuprita-pro@users.noreply.github.com>
2022-01-27 09:27:44 +0530

    adding two number in another text box

---
+++ choice_price_adding.java
@@ -0,0 +1,28 @@
+  int a =0;
+        if(jCheckBox1.isSelected()==true)
+        {
+             a=a+15;
+        }
+        if(jCheckBox2.isSelected()==true)
+        {
+             a=a+30;
+        }
+         if(jCheckBox3.isSelected()==true)
+        {
+             a=a+30;
+        }
+         if(jCheckBox4.isSelected()==true)
+        {
+             a=a+25;
+        }
+         if(jCheckBox5.isSelected()==true)
+        {
+             a=a+15;
+        }
+         if(jCheckBox6.isSelected()==true)
+        {
+             a=a+20;
+        }
+         
+        jTextField1.setText(""+a);
+        
